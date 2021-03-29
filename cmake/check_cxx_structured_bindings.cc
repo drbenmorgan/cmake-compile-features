f3bf1b11fef0b73127133fb92a7ca9e557d02134
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2021-03-29 12:30:48 +0100

    Additional structured binding check

--- cmake/check_cxx_structured_bindings.cc
+++ cmake/check_cxx_structured_bindings.cc
@@ -1,8 +1,16 @@
 // Check that C++ Structured Bindings are supported
 // - https://en.cppreference.com/w/cpp/language/structured_binding
 
+struct MyStruct {
+  int a = 42;
+  double b = 3.14;
+};
+
 int main() {
   // Array binding
   int a[2] = {42, 24};
   auto [x, y] = a;
+
+  // Struct binding
+  auto [n, m] = MyStruct{};
 }
